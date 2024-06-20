9637d7a8d73cba7e88ea03e8ec2c9a2bdcd87515
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-20 20:58:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,88 @@
-require('dotenv').config(); // Load environment variables from .env file
+const { Client, GatewayIntentBits, ActivityType, MessageEmbed } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+const { handleWhitelistApplication } = require('./functions/whitelist');
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ 
-    intents: [
-        Intents.FLAGS.GUILDS,           // Enable the GUILDS intent
-        Intents.FLAGS.GUILD_MESSAGES    // Enable the GUILD_MESSAGES intent
-    ]
+const client = new Client({
+  intents: Object.values(GatewayIntentBits),
 });
-require('discord-buttons')(client);
 
-// Import functions
-const applyFunction = require('./functions/apply');
+const prefix = '!';
+
+client.commands = new Map();
+
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on('messageCreate', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName);
+
+  if (command) {
+    try {
+      command.execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply('There was an error trying to execute that command!');
+    }
+  }
+});
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const indexPath = path.join(__dirname, 'index.html');
+  res.sendFile(indexPath);
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+});
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log('\x1b[32m%s\x1b[0m', '|    🍔 Bot logged in successfully!');
+    console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
+    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+    console.log('\x1b[36m%s\x1b[0m', `|    🏡 Bot is in ${client.guilds.cache.size} servers`);
+  } catch (error) {
+    console.error('\x1b[31m%s\x1b[0m', '❌ Failed to log in:', error);
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Login, Restarting Process...');
+    process.kill(1);
+  }
+}
 
 client.once('ready', () => {
-    console.log('Bot is online!');
+  setTimeout(() => {
+    console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
+    client.user.setPresence({
+      activities: [{ name: `Watching You !`, type: ActivityType.Custom }],
+      status: 'dnd',
+    });
+  }, 2000);
 });
 
-// Event listener for button clicks
-client.on('clickButton', async (button) => {
-    if (button.id === 'apply_button') {
-        await applyFunction.handleApply(client, button);
-    }
+client.on('messageCreate', (message) => {
+  handleWhitelistApplication(client, message);
 });
 
-// Login using environment variable
-client.login(process.env.DISCORD_TOKEN);
+login();
+
+setInterval(() => {
+  if (!client || !client.user) {
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Logged in, Restarting Process...');
+    process.kill(1);
+  }
+}, 15000);
+
+module.exports = client;
