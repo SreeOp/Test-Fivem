78cf9f693b397d44b5ba82f1666fb8142a143621
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-21 18:05:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.commands = new Collection();
+
+// Load commands
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
+
+// Load functions
+const functionsPath = path.join(__dirname, 'functions');
+const functionFiles = fs.readdirSync(functionsPath).filter(file => file.endsWith('.js'));
+
+for (const file of functionFiles) {
+    const filePath = path.join(functionsPath, file);
+    const func = require(filePath);
+    func(client);
+}
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
