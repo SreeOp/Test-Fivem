014c905c2f5db755be0d60d478a4c3e64620e226
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-21 18:18:39 +0530

    deploy-commands.js

--- functions/deploy-commands.js
+++ functions/deploy-commands.js
@@ -2,18 +2,18 @@ const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 
-const commands = [];
-const commandsPath = './commands';
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+module.exports = async (client) => {
+    const commands = [];
+    const commandsPath = './commands';
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-    const command = require(`../commands/${file}`);
-    commands.push(command.data.toJSON());
-}
+    for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+    const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-(async () => {
     try {
         console.log('Started refreshing application (/) commands.');
 
@@ -26,4 +26,4 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
     } catch (error) {
         console.error(error);
     }
-})();
+};
