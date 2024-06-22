d0d14f98f8d0d614be1677f19de2d7448238150d
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-22 06:58:18 +0530

    index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const TOKEN = process.env.DISCORD_TOKEN;
 // Event listener for when the bot is ready
 client.once('ready', () => {
     console.log('Bot is ready');
+    // Replace 'CHANNEL_ID' with the ID of the channel where you want to send the application message
+    sendApplicationMessage(client, '1253323014003757189');
 });
 
 // Event listener for handling interactions
@@ -29,8 +31,6 @@ client.on('interactionCreate', async (interaction) => {
 // Log in to Discord with your app's token
 client.login(TOKEN).then(() => {
     console.log('Logged in to Discord');
-    // Replace 'CHANNEL_ID' with the ID of the channel where you want to send the application message
-    sendApplicationMessage(client, '1253323014003757189');
 }).catch((error) => {
     console.error('Error logging in to Discord:', error);
 });
