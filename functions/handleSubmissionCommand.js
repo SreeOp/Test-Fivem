59499349d10de2c6c4e582731252a0b17aa1b03c
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-22 18:48:04 +0530

    Delete functions/handleSubmissionCommand.js

--- functions/handleSubmissionCommand.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (interaction, applicationSubmissionChannelId) => {
-    const channel = interaction.options.getChannel('channel');
-    if (channel) {
-        applicationSubmissionChannelId = channel.id;
-        await interaction.reply({ content: `Submission channel has been set to ${channel}`, ephemeral: true });
-    } else {
-        await interaction.reply({ content: 'Failed to find or access the specified channel.', ephemeral: true });
-    }
-};
