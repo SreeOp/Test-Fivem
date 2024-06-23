dfc1e50db80a63895ca55f9e2be5af31c27eb5fd
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-23 09:55:34 +0530

    say.js

---
+++ commands/say.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('say')
+        .setDescription('Repeats the message you provide')
+        .addStringOption(option => 
+            option.setName('message')
+                .setDescription('The message to repeat')
+                .setRequired(true)),
+    async execute(interaction) {
+        const message = interaction.options.getString('message');
+        await interaction.reply(message);
+    },
+};
