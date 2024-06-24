bdfe3c8b5275644bcc7a7d2ac38fe40d7add16fd
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-24 19:59:34 +0530

    index.js

--- index.js
+++ index.js
@@ -155,27 +155,32 @@ client.on('interactionCreate', async (interaction) => {
 
         if (member) {
             let embed;
+            let roleID;
+
             if (interaction.customId === 'acceptButton') {
                 embed = new EmbedBuilder()
                     .setTitle('Application Update')
                     .setDescription('Your application status: Accepted')
                     .setColor('#00ff00')
                     .setImage('https://cdn.discordapp.com/attachments/1056903195961610275/1254445277759148172/096ff227-e675-4307-a969-e2aac7a4c7ba-2.png?ex=667984b4&is=66783334&hm=0a486fb3dd9f322232f005efc1ebb1ce88e32eef1469278307d11a8c4aef7571&'); // Replace with your image URL
-                const role = interaction.guild.roles.cache.find(r => r.name === 'Whitelisted');
-                if (role) member.roles.add(role);
+                roleID = '1253347204601741342'; // Replace with your role ID for accepted applications
             } else if (interaction.customId === 'pendingButton') {
                 embed = new EmbedBuilder()
                     .setTitle('Application Update')
                     .setDescription('Your application status: Pending')
                     .setColor('#ffff00')
                     .setImage('https://cdn.discordapp.com/attachments/1056903195961610275/1254445277759148172/096ff227-e675-4307-a969-e2aac7a4c7ba-2.png?ex=667984b4&is=66783334&hm=0a486fb3dd9f322232f005efc1ebb1ce88e32eef1469278307d11a8c4aef7571&'); // Replace with your image URL
+                roleID = '1253347271718735882'; // Replace with your role ID for pending applications
             } else if (interaction.customId === 'rejectButton') {
                 embed = new EmbedBuilder()
                     .setTitle('Application Update')
                     .setDescription('Your application status: Rejected')
                     .setColor('#ff0000')
                     .setImage('https://cdn.discordapp.com/attachments/1056903195961610275/1254445277759148172/096ff227-e675-4307-a969-e2aac7a4c7ba-2.png?ex=667984b4&is=66783334&hm=0a486fb3dd9f322232f005efc1ebb1ce88e32eef1469278307d11a8c4aef7571&'); // Replace with your image URL
+                roleID = '1254774082445115432'; // Replace with your role ID for rejected applications
             }
+
+            await member.roles.add(roleID);
             await member.send({ embeds: [embed] });
             await interaction.deferUpdate();
         } else {
